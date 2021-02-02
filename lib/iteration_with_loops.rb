2666682a435451d63a9dae99eeb29485ee264f2a
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-02 22:44:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,12 @@
 def find_even_values(src)
  first_index = 0 
  while first_index < src.count do 
- second_iindex  
+ 
+ second_index = 0 
+ while second_index < src[first_index].count do 
+   
+   second_index +=1
+ end
+ first_index +=1
 end
 end
